b6325eef1eb56f170d3882681a96aa87242a6ec9
kaviyasree26 <150135134+kaviyasree26@users.noreply.github.com>
2024-04-29 11:43:26 +0530

    Add files via upload

---
+++ day-10/class_exercise/ex2/src/main/java/com/example/q2/repository/CustomerRepo.java
@@ -0,0 +1,13 @@
+package com.example.q2.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.q2.model.Customer;
+
+public interface CustomerRepo extends JpaRepository<Customer,Integer>{
+    @Query("select c from Customer c where c.city=?1")
+    List<Customer> findByCity(String city);
+} 
